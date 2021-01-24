219671634647ce8185d29e08f180c5f8c8b5448d
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-23 20:30:26 -0400

    test: prevent Login to call submit multiple times

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -115,4 +115,19 @@ describe('Login', () => {
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
     cy.url().should('eq', `${baseUrl}`)
   })
+
+  it('Should prevent multiple submits', () => {
+    cy.route({
+      method: 'post',
+      url: /login/,
+      status: 200,
+      response: {
+        accessToken: faker.random.uuid()
+      }
+    }).as('request')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').dblclick()
+    cy.get('@request.all').should('have.length', 1)
+  })
 })
