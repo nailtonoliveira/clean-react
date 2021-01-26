48f36641494d37d23ece09e2460a49ebbc8c26db
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-26 11:51:24 -0400

    test: ensure RemoteLoadSurveyList returns an empty list on 204

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -66,4 +66,13 @@ describe('RemoteLoadSurveyList', () => {
     const surveyList = await sut.loadAll()
     expect(surveyList).toEqual(httpResult)
   })
+
+  test('Should return a list of SurveyModel if HttpGetClient resturns 200', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.noContent
+    }
+    const surveyList = await sut.loadAll()
+    expect(surveyList).toEqual([])
+  })
 })
