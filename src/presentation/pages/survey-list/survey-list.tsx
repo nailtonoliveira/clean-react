fdbb8e16508b3f305d9516b8681372603000815a
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-26 19:23:18 -0400

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -22,28 +22,6 @@ const SurveyList = (): JSX.Element => {
             <footer>Ver Resultado</footer>
           </li>
           <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName="thumbUp"/>
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-              <p>Qual é seu Framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName="thumbUp"/>
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-              <p>Qual é seu Framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
           </li>
         </ul>
       </div>
