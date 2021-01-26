cd096652c8f055dfdfa729640613f84a024806a8
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-26 10:23:46 -0400

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -28,9 +28,14 @@ export const mockPostRequest = (): HttpPostParams => ({
   body: faker.random.objectElement()
 })
 
-export class HttpGetClientSpy implements HttpGetClient {
+export class HttpGetClientSpy<ResponseType> implements HttpGetClient<ResponseType> {
   url: string
-  async get (params: HttpGetParams): Promise<void> {
+  response: HttpResponse<ResponseType> = {
+    statusCode: HttpStatusCode.ok
+  }
+
+  async get (params: HttpGetParams): Promise<HttpResponse<ResponseType>> {
     this.url = params.url
+    return this.response
   }
 }
