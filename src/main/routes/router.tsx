36aba47af1c4f969a26a6584b1bb0d56d85d0c63
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-27 14:07:16 -0400

    feat: ensure PrivateRoute renders components if token is not empty

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -4,13 +4,14 @@ import { makeLogin } from '../factories/pages/login/login-factory'
 import { makeSignup } from '../factories/pages/signup/signup-factory'
 import { SurveyList } from '@/presentation/pages'
 import { ApiContext } from '@/presentation/contexts'
-import { setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
 
 const Router: React.FC = () => {
   return (
     <ApiContext.Provider
       value={{
-        setCurrentAccount: setCurrentAccountAdapter
+        setCurrentAccount: setCurrentAccountAdapter,
+        getCurrentAccount: getCurrentAccountAdapter
       }}
     >
       <BrowserRouter>
