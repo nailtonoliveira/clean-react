930e56b23761ab6feabd7f1e1cacd8890203c1f3
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-28 16:02:47 -0400

    feat: ensure Logout calls setCurrentAccount with undefined

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,26 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
 import { Logo } from '@/presentation/components'
 import Styles from './header-styles.scss'
+import { ApiContext } from '@/presentation/contexts'
+import { useHistory } from 'react-router-dom'
 
 const Header = (): JSX.Element => {
+  const { setCurrentAccount } = useContext(ApiContext)
+  const history = useHistory()
+
+  const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    event.preventDefault()
+    setCurrentAccount(undefined)
+    history.replace('/login')
+  }
+
   return (
     <header className={Styles.headerWrap}>
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
           <span>Nailton</span>
-          <a href="#">Sair</a>
+          <a data-testid="logout" href="#" onClick={logout}>Sair</a>
         </div>
       </div>
     </header>
