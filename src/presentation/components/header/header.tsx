a6ac3c3ff6ffb2a3c622af7e5c3b7104c2ab9dbc
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-28 16:14:57 -0400

    feat: ensure Header render username correctly

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -5,7 +5,7 @@ import { ApiContext } from '@/presentation/contexts'
 import { useHistory } from 'react-router-dom'
 
 const Header = (): JSX.Element => {
-  const { setCurrentAccount } = useContext(ApiContext)
+  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext)
   const history = useHistory()
 
   const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
@@ -19,7 +19,7 @@ const Header = (): JSX.Element => {
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
-          <span>Nailton</span>
+          <span data-testid="username">{getCurrentAccount().name}</span>
           <a data-testid="logout" href="#" onClick={logout}>Sair</a>
         </div>
       </div>
