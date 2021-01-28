f9281d0b7c61328cf79457cfb3318ea541059110
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-28 14:55:44 -0400

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/add-account/remote-add-account-factory.ts
+++ src/main/factories/usecases/add-account/remote-add-account-factory.ts
@@ -1,5 +1,4 @@
-import { makeApiUrl } from '@/main/factories/http/api-url-factory'
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
+import { makeApiUrl, makeAxiosHttpClient } from '@/main/factories/http'
 import { RemoteAddAccount } from '@/data/usecases/add-account/remote-add-account'
 import { AddAccount } from '@/domain/usecases'
 
