7591a1fa96a3fece255b6f84a58d5f4e768c1896
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-29 11:53:15 -0400

    test: ensure SurveyList logout on AccessDeniedError

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -1,7 +1,4 @@
-import * as FormHelpers from '../support/form-helpers'
 import * as Helpers from '../support/helpers'
-import * as Http from '../support/login-mocks'
-import faker from 'faker'
 
 describe('PrivateRoutes', () => {
   it('Should logout if survey-list has no token', () => {
