1db70c9208b696386c77001b96c3b9d6675c2914
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-29 11:50:18 -0400

    test: ensure SurveyList present error on UnexpectedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,19 @@
+
+import * as Helpers from '../support/helpers'
+import * as Http from '../support/survey-list-mocks'
+import faker from 'faker'
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    Helpers.setLocalStorageItem('account', {
+      accessToken: faker.random.uuid(),
+      name: faker.name.findName()
+    })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+})
